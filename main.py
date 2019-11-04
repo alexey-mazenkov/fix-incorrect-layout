5158091fece7b509c0445f2a4b709fe27a34b616
alexey-mazenkov <mazenkovof@gmail.com>
2019-11-04 17:23:55 +0700

    Determining the keyboard layout of the typed text.

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 # Guard the wrong layout.
 # Developer - Mazenkov A.
+import dictionary as dt
 
 print('Введите текст, который нужно исправить'.center(100, '-'))
 text = input()
+check = text.replace(' ', '')
 
-while not text.isalnum():
-     print('Ошибка. Попробуйте еще раз.'.center(100, '-'))
-     print('Введите текст, который нужно исправить'.center(100, '-'))
-     text = input()
+while not check.isalnum():
+    print('Ошибка. Попробуйте еще раз.'.center(100, '-'))
+    print('Введите текст, который нужно исправить'.center(100, '-'))
+    text = input()
+
+text = text.capitalize().lstrip()
+
+if text[0] in dt.dict_EN:
+    print('English'.center(100, '-'))
+
+elif text[0] in dt.dict_RU:
+     print('Русский'.center(100, '-'))
