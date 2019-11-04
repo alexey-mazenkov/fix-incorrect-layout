adcec331e5c71cb984ba823c3a166ebc97d9026a
alexey-mazenkov <mazenkovof@gmail.com>
2019-11-04 22:00:55 +0700

    Master

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-# Guard the wrong layout.
+# Guard the wrong keyboard layout.
 # Developer - Mazenkov A.
 import dictionary as dt
-
 
 while True:
 
     print('Введите текст, который нужно исправить'.center(100, '-'))
     text = input()
 
-    while not text.isalpha():
+    check = ''
+    for _ in range(len(text)):
+        check += text[_]
+        if text[_] in dt.symbol:
+            check = check.replace(text[_], '')
+
+    while not check.isalpha():
         print('Ошибка. Попробуйте еще раз.'.center(100, '-'))
         print('Введите текст, который нужно исправить'.center(100, '-'))
         text = input()
+        check = ''
+        for _ in range(len(text)):
+            check += text[_]
+            if text[_] in dt.symbol:
+                check = check.replace(text[_], '')
 
     text = text.upper().lstrip()
 
