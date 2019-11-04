ceff80058df0fc000de56b69ebd26f78810b166d
alexey-mazenkov <mazenkovof@gmail.com>
2019-11-04 16:44:10 +0700

    Added input form.

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 # Guard the wrong layout.
 # Developer - Mazenkov A.
+
+print('Введите текст, который нужно исправить'.center(100, '-'))
+text = input()
+
+while not text.isalnum():
+     print('Ошибка. Попробуйте еще раз.'.center(100, '-'))
+     print('Введите текст, который нужно исправить'.center(100, '-'))
+     text = input()
