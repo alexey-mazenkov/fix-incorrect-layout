6cc795ae54a5a17944bccf5ff44399c0a8767cae
alexey-mazenkov <mazenkovof@gmail.com>
2019-11-04 20:33:58 +0700

    Complete verification of input data.

--- main.py
+++ main.py
@@ -2,19 +2,34 @@
 # Developer - Mazenkov A.
 import dictionary as dt
 
-print('Введите текст, который нужно исправить'.center(100, '-'))
-text = input()
-check = text.replace(' ', '')
 
-while not check.isalnum():
-    print('Ошибка. Попробуйте еще раз.'.center(100, '-'))
+while True:
+
     print('Введите текст, который нужно исправить'.center(100, '-'))
     text = input()
 
-text = text.capitalize().lstrip()
+    while not text.isalpha():
+        print('Ошибка. Попробуйте еще раз.'.center(100, '-'))
+        print('Введите текст, который нужно исправить'.center(100, '-'))
+        text = input()
 
-if text[0] in dt.dict_EN:
-    print('English'.center(100, '-'))
+    text = text.upper().lstrip()
 
-elif text[0] in dt.dict_RU:
-     print('Русский'.center(100, '-'))
+    if text[0] in dt.dict_EN:
+        letter_ru = ''
+        for i in range(len(text)):
+            letter_en = dt.dict_EN[text[i]]
+            letter_ru += dt.dict_ru[letter_en]
+        print(letter_ru.capitalize())
+
+    elif text[0] in dt.dict_RU:
+        letter_en = ''
+        for i in range(len(text)):
+            letter_ru = dt.dict_RU[text[i]]
+            letter_en += dt.dict_en[letter_ru]
+        print(letter_en.capitalize())
+
+    print('Повторить ещё раз? (y/n)'.center(100, '-'))
+    cont = input()
+    if cont == 'n':
+        break
