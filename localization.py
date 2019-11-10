3a0bcc3b33eac7d944050f172025c08f991c6c44
alexey-mazenkov <mazenkovof@gmail.com>
2019-11-11 01:30:31 +0700

    Language selection adjusted.

--- localization.py
+++ localization.py
@@ -1,9 +1,12 @@
-print('Choose the language (ru, en, cn, fr)'.center(100, '-'))
+print('Choose the language (ru, en)'.center(100, '-'))
 print('RU - Русский '.center(100, '-'))
 print('EN - English'.center(100, '-'))
-print('CN - 中文'.center(98, '-'))
-print('FR - Français'.center(100, '-'))
 lang_choose = input()
+
+while lang_choose.lower() != 'ru' and lang_choose.lower() != 'en':
+    print('You have selected an unsupported language'.center(100, '-'))
+    print('Try again'.center(100, '-'))
+    lang_choose = input()
 
 if lang_choose.lower() == 'ru':
     start_message = 'Введите текст, который нужно исправить'
@@ -15,12 +18,3 @@ elif lang_choose.lower() == 'en':
     error_message = 'Mistake. Try again'
     try_again = 'To repeat again? (y/n)'
 
-elif lang_choose.lower() == 'cn':
-    start_message = '输入要更正的文本'
-    error_message = '错误 再试一次'
-    try_again = '再重复一次？ (y/n)'
-
-elif lang_choose.lower() == 'fr':
-    start_message = 'Saisissez le texte à corriger'
-    error_message = 'Erreur. Essayez à nouveau'
-    try_again = 'Répéter encore une fois? (y / n)'
